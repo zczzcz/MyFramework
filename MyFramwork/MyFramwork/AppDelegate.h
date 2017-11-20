e705765c690119b3b9428d94f4e1f99bc2fd4f98
zhangcz <zcz1513@163.com>
2017-11-20 11:24:53 +0800

    add new project

---
+++ MyFramwork/MyFramwork/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyFramwork
+//
+//  Created by zhangcz on 2017/11/20.
+//  Copyright © 2017年 zhangcz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyFramwork/MyFramwork/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyFramwork
+//
+//  Created by zhangcz on 2017/11/20.
+//  Copyright © 2017年 zhangcz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
